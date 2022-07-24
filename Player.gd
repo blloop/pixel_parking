ddd8ebbdb3068c6d1a07f67dfe2c92ae487ba7bf
blloop <42186044+blloop@users.noreply.github.com>
2022-07-23 17:40:15 -0700

    Create structure, basic scenes

---
+++ Player.gd
@@ -0,0 +1,17 @@
+extends Area2D
+signal bump
+
+
+# Declare member variables here. Examples:
+export var acceleration = 2
+export var max_speed = 5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
